6530621d943d0e0d0af89668038d74f306f4be2c
muicoder <muicoder@gmail.com>
2025-05-19 11:03:44 +0800

    k3s-vip

--- pkg/oci/containerd.go
+++ pkg/oci/containerd.go
@@ -489,6 +489,10 @@ func parseContentRegistries(l map[string]string) []string {
 func createFilters(mirroredRegistries []url.URL) ([]string, []string, []string) {
 	registryHosts := []string{}
 	for _, registry := range mirroredRegistries {
+		if registry.Host == "*" || registry.Host == "_default" {
+			registryHosts = []string{".+"}
+			break
+		}
 		registryHosts = append(registryHosts, strings.ReplaceAll(registry.Host, `.`, `\\.`))
 	}
 	imageFilter := fmt.Sprintf(`name~="^(%s)/"`, strings.Join(registryHosts, "|"))
