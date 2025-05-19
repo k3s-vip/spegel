960178521521768ae8f9ea05c71d1cf8975a6600
muicoder <muicoder@gmail.com>
2025-05-19 09:57:04 +0800

    AIO for build

--- pkg/oci/containerd.go
+++ pkg/oci/containerd.go
@@ -207,15 +207,15 @@ func (c *Containerd) Features(ctx context.Context) (Feature, error) {
 }
 
 func featuresForVersion(version string) (Feature, error) {
-	v, err := utilversion.Parse(version)
+	v, err := utilversion.ParseGeneric(version)
 	if err != nil {
 		return 0, fmt.Errorf("could not parse version %s: %w", version, err)
 	}
 	feats := Feature(0)
-	if v.LessThan(utilversion.MustParse("2.0")) {
+	if v.LessThan(utilversion.MustParseGeneric("2.0")) {
 		feats.Set(FeatureConfigCheck)
 	}
-	if v.AtLeast(utilversion.MustParse("2.1")) {
+	if v.AtLeast(utilversion.MustParseGeneric("2.1")) {
 		feats.Set(FeatureContentEvent)
 	}
 	return feats, nil
