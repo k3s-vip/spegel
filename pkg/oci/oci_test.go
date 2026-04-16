083569012cfc012c4aa285f81d1b68251d2dbbeb
muicoder <muicoder@gmail.com>
2026-04-16 12:22:09 +0800

    downgrade containerd

--- pkg/oci/oci_test.go
+++ pkg/oci/oci_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/containerd/containerd/v2/core/images"
+	"github.com/containerd/containerd/images"
 	"github.com/opencontainers/go-digest"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/stretchr/testify/require"
